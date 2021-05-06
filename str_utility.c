d290c6e71bd12dbf1d93c45cd97168d84a96eb39
Frans Holwerda <fransholwerda@Franss-MacBook-Pro.local>
2021-05-06 12:04:41 +0200

    laptop push

--- str_utility.c
+++ str_utility.c
@@ -6,28 +6,11 @@
 /*   By: fholwerd <fholwerd@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/08 09:52:25 by fholwerd      #+#    #+#                 */
-/*   Updated: 2021/04/15 12:48:07 by fholwerd      ########   odam.nl         */
+/*   Updated: 2021/05/02 13:42:17 by fholwerd      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return ((char *)&s[i]);
-		i++;
-	}
-	if (c == 0)
-		return ((char *)&s[i]);
-	return (NULL);
-}
-
 
 size_t	count_digits(const char *str, long int i)
 {
